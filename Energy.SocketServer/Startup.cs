72e2961ec0a604e878938771abd2b435b07369a9
张奔 <zhangbenclass3@126.com>
2019-05-31 22:23:09 +0800

    添加桌面启动项目，用来启动数据接收服务于数据解析服务，未完成

---
+++ Energy.SocketServer/Startup.cs
@@ -0,0 +1,41 @@
+﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Energy.SocketServer
+{
+    public class Startup
+    {
+        public static void StartSocket()
+        {
+            Runtime.m_Bootstrap = BootstrapFactory.CreateBootstrap();
+
+            if (!Runtime.m_Bootstrap.Initialize())
+            {
+                Console.WriteLine("{0} -> Failed to initialize!", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                Console.ReadKey();
+                return;
+            }
+
+            var result = Runtime.m_Bootstrap.Start();
+
+            Console.WriteLine("{1} -> Start result: {0}!", result, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+
+            if (result == StartResult.Failed)
+            {
+                Console.WriteLine("{0} -> Failed to start!", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                Console.ReadKey();
+                return;
+            }
+        }
+
+        public static void StopSocket()
+        {
+
+        }
+    }
+}
---
+++ Energy.Start/Main.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Energy.Start
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolRunLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.startTimeLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.rtbServerLog = new System.Windows.Forms.RichTextBox();
+            this.rtbErrorLog = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.rtbSocketLog = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(59, 47);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(77, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(59, 47);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(1, 65);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 360);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.rtbServerLog);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(791, 331);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "系统日志";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.rtbErrorLog);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(791, 331);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "错误日志";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolRunLabel,
+            this.startTimeLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolRunLabel
+            // 
+            this.toolRunLabel.Name = "toolRunLabel";
+            this.toolRunLabel.Size = new System.Drawing.Size(68, 17);
+            this.toolRunLabel.Text = "启动时间：";
+            // 
+            // startTimeLabel
+            // 
+            this.startTimeLabel.Name = "startTimeLabel";
+            this.startTimeLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // rtbServerLog
+            // 
+            this.rtbServerLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbServerLog.Location = new System.Drawing.Point(3, 3);
+            this.rtbServerLog.Name = "rtbServerLog";
+            this.rtbServerLog.Size = new System.Drawing.Size(785, 325);
+            this.rtbServerLog.TabIndex = 0;
+            this.rtbServerLog.Text = "";
+            // 
+            // rtbErrorLog
+            // 
+            this.rtbErrorLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbErrorLog.Location = new System.Drawing.Point(3, 3);
+            this.rtbErrorLog.Name = "rtbErrorLog";
+            this.rtbErrorLog.Size = new System.Drawing.Size(785, 325);
+            this.rtbErrorLog.TabIndex = 0;
+            this.rtbErrorLog.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.rtbSocketLog);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(791, 331);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "连接日志";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // rtbSocketLog
+            // 
+            this.rtbSocketLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbSocketLog.Location = new System.Drawing.Point(3, 3);
+            this.rtbSocketLog.Name = "rtbSocketLog";
+            this.rtbSocketLog.Size = new System.Drawing.Size(785, 325);
+            this.rtbSocketLog.TabIndex = 0;
+            this.rtbSocketLog.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Main";
+            this.Text = "数据服务";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolRunLabel;
+        private System.Windows.Forms.ToolStripStatusLabel startTimeLabel;
+        private System.Windows.Forms.RichTextBox rtbServerLog;
+        private System.Windows.Forms.RichTextBox rtbErrorLog;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox rtbSocketLog;
+    }
+}
+
